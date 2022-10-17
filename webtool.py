57bffb2c6a8ba62e14876f1defcfa07705b5f274
haxorpoint <85513078+haxorpoint@users.noreply.github.com>
2022-10-17 15:43:21 +0530

    Add files via upload

---
+++ webtool.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+class WebScraping:
+    def __init__(self):
+        print("""
+                #################  HAXORPOINT  #################
+                
+                >>>> Name: Professor
+                
+                #################  HAXORPOINT  #################""")
+
+obj = WebScraping()
